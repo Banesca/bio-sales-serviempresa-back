1fe241aaf280b81e1c2465afdf803caaad61973e
andri suarez <ajosuesh@gmail.com>
2023-08-04 10:57:25 -0500

    feat: add TDC  module

---
+++ components/tdc/useTdc.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import { useRequest } from '../../hooks/useRequest';
+import axios from 'axios';
+import { ip } from '../../util/environment';
+import { Form, message } from 'antd';
+
+export const useTdc = () => {
+	const { requestHandler } = useRequest();
+	const [actualTdc, setActualTdc] = useState(0);
+	const [tdc, setTdc] = useState({});
+	const [form] = Form.useForm();
+	const [loading, setLoading] = useState(false);
+	const token2 =
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZFVzZXIiOiI1NWE1NDAwOGFkMWJhNTg5YWEyMTBkMjYyOWMxZGY0MSIsImlhdCI6MTU5MDk0NTY4NH0.V-9GPQoET85cHM3YGSOLNYEKOw_ajQ7xn8bA6h_Xv60';
+
+	const getTdc = async () => {
+		try {
+			const api_port = localStorage.getItem('apiPort')
+				? localStorage.getItem('apiPort')
+				: generalContext.api_port;
+			const { data } = await axios.get(`${ip}:${api_port}/api/v2/param`, {
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+					Authorization: 'Bearer ' + token2,
+				},
+			});
+			if (data.success) {
+				let tdc = data.response[52];
+				console.log(tdc);
+				setActualTdc(tdc.param);
+				setTdc(tdc);
+				form.setFieldValue('param', tdc.param);
+			}
+		} catch (error) {
+			message.error('No fue posible cargar la tasa de cambio');
+		}
+	};
+
+	const updateTdc = async (e) => {
+		try {
+			setLoading(true);
+			const api_port = localStorage.getItem('apiPort')
+				? localStorage.getItem('apiPort')
+				: generalContext.api_port;
+
+			const { data } = await axios.put(
+				`${ip}:${api_port}/api/v2/param/update`,
+				{ ...e, idParam: tdc.idParam },
+				{
+					headers: {
+						'Access-Control-Allow-Origin': '*',
+						Authorization: 'Bearer ' + token2,
+					},
+				}
+			);
+
+			if (data.success) getTdc();
+			setLoading(false);
+		} catch (error) {
+			message.error('No fue posible guardar la tasa de cambio');
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		getTdc();
+	}, []);
+
+	return {
+		getTdc,
+		actualTdc,
+		updateTdc,
+		updateTdc,
+		form,
+		loading,
+	};
+};
--- pages/dashboard/tdc/index.jsx
+++ pages/dashboard/tdc/index.jsx
@@ -1,9 +1,66 @@
+import { Button, Card, Col, Form, Input, Row } from 'antd';
 import DashboardLayout from '../../../components/shared/layout';
+import Title from '../../../components/shared/title';
+import { useTdc } from '../../../components/tdc/useTdc';
 
 export default function Tdc() {
+	const { actualTdc, updateTdc, form, loading } = useTdc();
+
 	return (
 		<DashboardLayout>
-			<p>Tasa de cambio</p>
+			<div className="gap-3 flex flex-col items-center justify-center p-4 m-4">
+				<Title title={'Tasa de cambios'}></Title>
+				<Row className="w-[95%]" gutter={10}>
+					<Col span={12}>
+						<Card className="shadow-lg">
+							<h1 className="text-3xl text-center">Tasa actual</h1>
+							<h2 className="text-3xl text-center">
+								<b>{actualTdc}</b>
+							</h2>
+						</Card>
+					</Col>
+					<Col span={12}>
+						<Card className="shadow-lg">
+							<h1 className="text-3xl text-center">Actualizar tasa</h1>
+
+							<Row justify={'center'}>
+								<Col span={20}>
+									<Form
+										layout="vertical"
+										form={form}
+										onFinish={updateTdc}
+										className="p-6"
+									>
+										<Form.Item
+											label="Nueva tasa de cambio"
+											initialValue={actualTdc}
+											rules={[
+												{ required: true, message: 'La tasa es requerida' },
+											]}
+											name="param"
+										>
+											<Input value={actualTdc} size="large" />
+										</Form.Item>
+										<Form.Item>
+											<Row justify={'center'}>
+												<Button
+													type="success"
+													block
+													htmlType="submit"
+													loading={loading}
+													size="large"
+												>
+													Guardar
+												</Button>
+											</Row>
+										</Form.Item>
+									</Form>
+								</Col>
+							</Row>
+						</Card>
+					</Col>
+				</Row>
+			</div>
 		</DashboardLayout>
 	);
 }
